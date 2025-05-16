087ac2261623cc083c26df460b159a9882757bf3
feabs <feabscapital@gmail.com>
2025-05-16 10:21:26 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(cors({
+  origin: '*', // allow all for now
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect MongoDB
